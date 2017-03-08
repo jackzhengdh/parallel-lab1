769c89ef31b92d8bc94c20d7e88674682f7f8f60
Duanhao Zheng <JackZheng@172-16-14-174.DYNAPOOL.NYU.EDU>
2017-03-08 13:22:15 -0500

    test read & print

--- test3.c
+++ test3.c
@@ -10,10 +10,10 @@ void Read_top(FILE* fp, int* n_p, int* local_n_p, double* err_p,
 void Print_top(int n, double err, int my_rank, MPI_Comm comm);
 void Allocate_arrays(double** local_A, double** local_b, double** local_x,
 	int n, int local_n, MPI_Comm comm);
-void Read_content(FILE* fp, double local_x[], int n, int local_n,
-	int my_rank, MPI_Comm comm);
-void Print_content(double local_x[], int n, int local_n,
-	int my_rank, MPI_Comm comm);
+void Read_content(FILE* fp, double local_A[], double local_b[], 
+	double local_x[], int n, int local_n, int my_rank, MPI_Comm comm);
+void Print_content(double local_A[], double local_b[], double local_x[],
+	int n, int local_n, int my_rank, MPI_Comm comm);
 
 int main(int argc, char* argv[]) {
 	double* local_A;
@@ -39,9 +39,9 @@ int main(int argc, char* argv[]) {
 
 	Read_top(fp, &n, &local_n, &err, my_rank, comm_sz, comm);
 	Allocate_arrays(local_A, local_b, local_x, n, local_n, comm);
-	Read_content(fp, local_x, n, local_n, my_rank, comm);
+	Read_content(fp, local_A, local_b, local_x, n, local_n, my_rank, comm);
 	Print_top(n, err, my_rank, comm);
-	Print_content(local_x, n, local_n, my_rank, comm);
+	Print_content(local_A, local_b, local_x, n, local_n, my_rank, comm);
 	if (my_rank == 0)
 		fclose(fp);
 	MPI_Finalize();
@@ -93,22 +93,39 @@ void Allocate_arrays(
 }
 void Read_content(
 	FILE* 		fp 			/* in  */,
+	double 		local_A[] 	/* out */,
+	double 		local_b[] 	/* out */,
 	double 		local_x[] 	/* out */,
 	int 		n 			/* in  */,
 	int 		local_n 	/* in  */,
 	int 		my_rank 	/* in  */,
 	MPI_Comm 	comm 		/* in  */) {
 
+	double* A = NULL;
+	double* b = NULL;
 	double* x = NULL;
 	int i, j;
 
 	if (my_rank == 0) {
+		A = malloc(n*n*sizeof(double));
+		b = malloc(n*sizeof(double));
 		x = malloc(n*sizeof(double));
 		printf("Logging in values of x\n");
 		for (i = 0; i < n; i++)
 			fscanf(fp, "%lf", &x[i]);
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++)
+				fscanf(fp, "%lf", &A[i*n+j]);
+			fscanf(fp, "%lf", &b[i]);
+		}
+		MPI_Scatter(A, n*local_n, MPI_DOUBLE, 
+			local_A, n*local_n, MPI_DOUBLE, 0, comm);
+		MPI_Scatter(b, local_n, MPI_DOUBLE, 
+			local_b, local_n, MPI_DOUBLE, 0, comm);
 		MPI_Scatter(x, local_n, MPI_DOUBLE, 
-			local_x, local_n, MPI_DOUBLE, 0, comm);
+			local_x, local_n, MPI_DOUBLE, 0, comm);		
+		free(A);
+		free(b);
 		free(x);
 	} else {
 		MPI_Scatter(x, local_n, MPI_DOUBLE, 
@@ -117,32 +134,51 @@ void Read_content(
 }
 
 void Print_content(
-	double 		local_x[] 	/* in */,
+	double 		local_A[] 	/* in */,
+	double 		local_b[] 	/* in */,
+	double 		local_x[] 	/* in */,	
 	int 		n 			/* in */,
 	int 		local_n 	/* in */, 
 	int 		my_rank 	/* in */,
 	MPI_Comm 	comm 		/* in */) {
 
+	double* A = NULL;
+	double* b = NULL;
 	double* x = NULL;
-	int i;
+	int i, j;
 
 	if (my_rank == 0) {
+		A = malloc(n*n*sizeof(double));
+		b = malloc(n*sizeof(double));
 		x = malloc(n*sizeof(double));
+		MPI_Gather(local_A, n*local_n, MPI_DOUBLE,
+			A, n*local_n, MPI_DOUBLE, 0, comm);
+		MPI_Gather(local_b, local_n, MPI_DOUBLE,
+			b, local_n, MPI_DOUBLE, 0, comm);
 		MPI_Gather(local_x, local_n, MPI_DOUBLE,
 			x, local_n, MPI_DOUBLE, 0, comm);
+		
 		printf("\nPrinting values of x\n");
 		for (i = 0; i < n; i++)
 			printf("%f ", x[i]);
-		printf("\n");
+		printf("\nPrinting content of matrix\n");
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++)
+				printf("%f ", A[i*n+j]);
+			printf("%f ", b[i]);
+		}
+		free(A);
+		free(b);
 		free(x);
 	} else {
+		MPI_Gather(local_A, n*local_n, MPI_DOUBLE,
+			A, n*local_n, MPI_DOUBLE, 0, comm);
+		MPI_Gather(local_b, local_n, MPI_DOUBLE,
+			b, local_n, MPI_DOUBLE, 0, comm);
 		MPI_Gather(local_x, local_n, MPI_DOUBLE,
 			x, local_n, MPI_DOUBLE, 0, comm);
 	}
 }
-
-
-
 
 
 
