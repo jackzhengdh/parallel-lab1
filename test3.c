fd555247e26c37ec38c14891bffb7719eb1d9868
Duanhao Zheng <JackZheng@Duanhaos-Macintosh.local>
2017-03-07 22:19:31 -0500

    test read & print

--- test3.c
+++ test3.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <mpi.h>
 
-void Read(int* n_p, int my_rank, MPI_Comm comm);
+void Read(FILE* fp, int* n_p, int my_rank, MPI_Comm comm);
 void Print(int n, int my_rank, MPI_Comm comm);
 
 int main(int argc, char* argv[]) {
@@ -11,19 +11,19 @@ int main(int argc, char* argv[]) {
 	int my_rank, comm_sz;
 	MPI_Comm comm;
 
-	// char *filename = argv[1];
-	// FILE* fp;
-	// if (my_rank == 0) {
-	// 	printf("my_rank = 0, opening file\n");
-	// 	fp = fopen(filename, "r");
-	// }
+	char *filename = argv[1];
+	FILE* fp;
+	if (my_rank == 0) {
+		printf("my_rank = 0, opening file\n");
+		fp = fopen(filename, "r");
+	}
 
 	MPI_Init(NULL, NULL);
 	comm = MPI_COMM_WORLD;
 	MPI_Comm_size(comm, &comm_sz);
 	MPI_Comm_rank(comm, &my_rank);
 
-	Read(&n, my_rank, comm);
+	Read(fp, &n, my_rank, comm);
 	Print(n, my_rank, comm);
 
 	MPI_Finalize();
@@ -31,14 +31,14 @@ int main(int argc, char* argv[]) {
 }
 
 void Read(
-	// FILE* 		fp 			/* in */,
+	FILE* 		fp 			/* in */,
 	int* 		n_p 		/* out */,
 	int 		my_rank 	/* in  */,
 	MPI_Comm 	comm 		/* in  */) {
 
 	if (my_rank == 0) {
 		printf("Reading number...\n");
-		scanf("%d", n_p);
+		fscanf(fp, "%d", n_p);
 	}
 	MPI_Bcast(n_p, 1, MPI_INT, 0, comm);
 }
