71c6d62484bdfbdaac125da762ab880b6d51129e
Duanhao Zheng <JackZheng@Duanhaos-Macintosh.local>
2017-03-09 03:22:07 -0500

    test end conditions

--- test3.c
+++ test3.c
@@ -357,9 +357,9 @@ void Update_x(
 			for (j = 0; j < n; j++)
 				printf("%f\n", x[j]);
 			printf("total number of iterations: %d\n", phase);
-			phase = -1;
 		}
-		MPI_Bcast(&phase, 1, MPI_INT, 0, comm);
+		if (cnt == 0)
+			phase = -1;
 	}
 }
 
