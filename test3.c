716cfbef47ca5bb952747a9b65c38345a3882d6a
Duanhao Zheng <JackZheng@Duanhaos-Macintosh.local>
2017-03-09 03:12:41 -0500

    test end conditions

--- test3.c
+++ test3.c
@@ -351,10 +351,10 @@ void Update_x(
 		if (cnt == 0 && my_rank == 0) {
 			printf("Enters if cnt == 0 stage..\n");
 			if (phase % 2 == 0) {
-				MPI_Allgather(local_y, local_n, MPI_DOUBLE,
+				MPI_Allgather(local_x, local_n, MPI_DOUBLE,
 					x, local_n, MPI_DOUBLE, comm);
 			} else {
-				MPI_Allgather(local_x, local_n, MPI_DOUBLE,
+				MPI_Allgather(local_y, local_n, MPI_DOUBLE,
 					x, local_n, MPI_DOUBLE, comm);
 			}
 			for (j = 0; j < n; j++)
